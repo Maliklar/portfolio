b4ab38ae06c38c5ac68e3b59e60c331da7074496
Malik Elbadri <Elbadrimalik@gmail.com>
2023-09-08 21:24:45 +0300

    Updated Hero section

--- src/components/Specific/Sections/HeroSection/index.tsx
+++ src/components/Specific/Sections/HeroSection/index.tsx
@@ -12,8 +12,7 @@ const HeroSection = () => {
     <header className={styles.container}>
       <div className={styles.mainContent}>
         <div className={styles.content}>
-          <h1 className={styles.header}>Hello,{" I'm "}Malik.</h1>
-
+          <h1 className={styles.header}>Hello,{" I'm "}Malik</h1>
           <p className={styles.subtitle}>
             I am a full stack web developer specialized in building and
             maintaining beautiful and accessible websites using various
@@ -41,23 +40,23 @@ const HeroSection = () => {
       </button>
       <div className={styles.footer}>
         <div className={styles.line} />
-        <div>
+        <div className={styles.gitHub}>
           <FaGithub />
         </div>
         <div className={styles.line} />
-        <div>
+        <div className={styles.linkedIn}>
           <FaLinkedin />
         </div>
         <div className={styles.line} />
-        <div>
+        <div className={styles.feMentor}>
           <SiFrontendmentor />
         </div>
         <div className={styles.line} />
-        <div>
+        <div className={styles.leetCode}>
           <SiLeetcode />
         </div>
         <div className={styles.line} />
-        <div>
+        <div className={styles.hr}>
           <SiHackerrank />
         </div>
         <div className={styles.line} />
